c4460af5736227eb25a1cd71220eea78443b2b64
Ode Ardika <104220059+odeardika@users.noreply.github.com>
2025-05-04 00:42:03 +0800

    feat: adding the skill in skill section

--- components/Section/Skills/SkillList/SkillList.tsx
+++ components/Section/Skills/SkillList/SkillList.tsx
@@ -1,26 +1,59 @@
-import SkillCard  from "@components/Section/Skills/SkillCard/SkillCard";
+import SkillCard from "@components/Section/Skills/SkillCard/SkillCard";
 
 const MLCat = [
-    {id : 0, name : "Python", level : "Intermediate", icon : "/assets/icons/pythonIcons.svg" },
-    {id : 1, name : "TensorFlow", level : "Intermediate", icon : '/assets/icons/tensorflowIcons.svg'},
-    {id : 2, name : "PyTorch", level : "Beginner ", icon : '/assets/icons/pytorchIcons.svg'}
+  { name: "Python", level: "Intermediate", icon: "/assets/icons/pythonIcon.svg" },
+  { name: "TensorFlow", level: "Intermediate", icon: '/assets/icons/tensorflowIcon.svg' },
+  { name: "PyTorch", level: "Beginner", icon: '/assets/icons/pytorchIcon.svg' }
+]
+const FECat = [
+  { name: "HTML", level: "Intermediate", icon: "/assets/icons/htmlIcon.svg" },
+  { name: "CSS", level: "Intermediate", icon: "/assets/icons/cssIcon.svg" },
+  { name: "Javascript", level: "Intermediate", icon: "/assets/icons/javascriptIcon.svg" },
+  { name: "React", level: "Intermediate", icon: "/assets/icons/reactIcon.svg" },
+  { name: "Bootstrap", level: "Beginner", icon: "/assets/icons/bootstrapIcon.svg" },
+  { name: "Tailwind", level: "Intermediate", icon: "/assets/icons/tailwindIcon.svg" },
+  { name: "Shadcn", level: "Beginner", icon: "/assets/icons/shadcnIcon.svg" },
+]
+
+const BECat = [
+  { name: "Flask", level: "Intermediate", icon: "/assets/icons/flaskIcon.svg" },
+  { name: "NodeJS", level: "Intermediate", icon: "/assets/icons/nodeIcon.svg" },
+  { name: "BunJS", level: "Beginner", icon: "/assets/icons/bunIcon.svg" },
+  { name: "Express", level: "Intermediate", icon: '/assets/icons/expressIcon.svg' },
+  { name: "MySQL", level: "Intermediate", icon: '/assets/icons/mysqlIcon.svg' },
+]
+
+const FSCat = [
+  { name: "NextJS", level: "Intermediate", icon: "/assets/icons/nextIcon.svg" },
+  { name: "PHP", level: "Intermediate", icon: "/assets/icons/phpIcon.svg" },
+  { name: "Laravel", level: "Intermediate", icon: '/assets/icons/laravelIcon.svg' },
+  { name: "Django", level: "Beginner ", icon: '/assets/icons/djangoIcon.svg' }
 ]
 
 const listOfSkill = {
-    ML : MLCat
+  ML: MLCat,
+  FE: FECat,
+  BE: BECat,
+  FS: FSCat,
+  ALL: MLCat.concat(FECat, BECat, FSCat)
 }
 
-export default function SkillList() {
+type skillListProps = {
+  select: "ML" | "FE" | "BE" | "FS" | "ALL";
+}
+export default function SkillList(props: skillListProps) {
+
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 gap-2 md:gap-8 lg:gap-10 px-10 lg:px-48 xl:px-48">
-        {listOfSkill["ML"].map((items) => (
-                <SkillCard 
-                key={items.id}
-                title={items.name} 
-                level={items.level} 
-                icon={items.icon}/>
-        ))}
+      {!(props.select in listOfSkill) ? "" :
 
+        (listOfSkill[`${props.select}`].map((items, index) => (
+          <SkillCard
+            key={index}
+            title={items.name}
+            level={items.level}
+            icon={items.icon} />
+        )))}
     </div>
   )
 }
--- components/Section/Skills/Skills.tsx
+++ components/Section/Skills/Skills.tsx
@@ -1,8 +1,7 @@
-'use client';
+"use client";
 import { useState } from "react";
 import CategoryComp from "./CategoryComp/CategoryComp";
 import SkillList from "./SkillList/SkillList";
-
 
 export default function Skills() {
     const [machileLearningCat, setMachileLearningCat] = useState(true);
@@ -10,85 +9,99 @@ export default function Skills() {
     const [backendCat, setBackendCat] = useState(false);
     const [fullstackCat, setFullstackCat] = useState(false);
     const [allCat, setAllCat] = useState(false);
+    const [selectedCat, setSelectedCat] = useState<
+        "ML" | "FE" | "BE" | "FS" | "ALL"
+    >("ML");
 
     const MLHandler = () => {
-        if (machileLearningCat) {
-            setMachileLearningCat(!machileLearningCat);
-        } else {
-            setBackendCat(false);
-            setFrontendCat(false);
-            setFullstackCat(false);
-            setAllCat(false);
-            setMachileLearningCat(!machileLearningCat);
-        }
+        setBackendCat(false);
+        setFrontendCat(false);
+        setFullstackCat(false);
+        setAllCat(false);
+        setMachileLearningCat(true);
+        setSelectedCat("ML");
     };
     const FrontendHandler = () => {
-        if (frontendCat) {
-            setFrontendCat(!frontendCat);
-        } else {
-            setBackendCat(false);
-            setMachileLearningCat(false);
-            setFullstackCat(false);
-            setAllCat(false);
-            setFrontendCat(!frontendCat);
-        }
+        setBackendCat(false);
+        setMachileLearningCat(false);
+        setFullstackCat(false);
+        setAllCat(false);
+        setFrontendCat(true);
+        setSelectedCat("FE");
     };
     const BackendHandler = () => {
-        if (backendCat) {
-            setBackendCat(!backendCat);
-        } else {
-            setFrontendCat(false);
-            setMachileLearningCat(false);
-            setFullstackCat(false);
-            setAllCat(false);
-            setBackendCat(!backendCat);
-        }
+        setFrontendCat(false);
+        setMachileLearningCat(false);
+        setFullstackCat(false);
+        setAllCat(false);
+        setBackendCat(true);
+        setSelectedCat("BE");
     };
     const FullstackHandler = () => {
-        if (fullstackCat) {
-            setFullstackCat(!fullstackCat);
-        } else {
-            setBackendCat(false);
-            setFrontendCat(false);
-            setMachileLearningCat(false);
-            setAllCat(false);
-            setFullstackCat(!fullstackCat);
-        }
+        setBackendCat(false);
+        setFrontendCat(false);
+        setMachileLearningCat(false);
+        setAllCat(false);
+        setFullstackCat(true);
+        setSelectedCat("FS");
     };
     const AllHandler = () => {
-        if (allCat) {
-            setAllCat(!allCat);
-        } else {
-            setBackendCat(false);
-            setFrontendCat(false);
-            setFullstackCat(false);
-            setMachileLearningCat(false);
-            setAllCat(!allCat);
-        }
+        setBackendCat(false);
+        setFrontendCat(false);
+        setFullstackCat(false);
+        setMachileLearningCat(false);
+        setAllCat(true);
+        setSelectedCat("ALL");
     };
-
 
     return (
         <section className="flex flex-col items-center justify-center gap-8 py-20">
-            <div className='flex flex-col gap-2 md:gap-4 items-center'>
-                <h2 className='font-semibold text-2xl md:text-4xl lg:text-5xl text-dark_blue'>My Skills</h2>
-                <p className='text-dark_blue font-light text-xs md:text-base lg:text-xl'>My Technical Level</p>
+            <div className="flex flex-col gap-2 md:gap-4 items-center">
+                <h2 className="font-semibold text-2xl md:text-4xl lg:text-5xl text-dark_blue">
+                    My Skills
+                </h2>
+                <p className="text-dark_blue font-light text-xs md:text-base lg:text-xl">
+                    My Technical Level
+                </p>
             </div>
 
-            <div className='category-skill'>
+            <div className="category-skill">
                 <ul className="flex gap-4 text-wrap justify-center my-2 flex-wrap">
-                    <CategoryComp onText="Machine Learning" offText="ML" condition={machileLearningCat} onClick={MLHandler}/>
-                    <CategoryComp onText="Frontend" offText="FE" condition={frontendCat} onClick={FrontendHandler}/>
-                    <CategoryComp onText="Backend" offText="BE" condition={backendCat} onClick={BackendHandler}/>
-                    <CategoryComp onText="Fullstack" offText="FS" condition={fullstackCat} onClick={FullstackHandler}/>
-                    <CategoryComp onText="ALL" offText="ALL" condition={allCat} onClick={AllHandler}/>
+                    <CategoryComp
+                        onText="Machine Learning"
+                        offText="ML"
+                        condition={machileLearningCat}
+                        onClick={MLHandler}
+                    />
+                    <CategoryComp
+                        onText="Frontend"
+                        offText="FE"
+                        condition={frontendCat}
+                        onClick={FrontendHandler}
+                    />
+                    <CategoryComp
+                        onText="Backend"
+                        offText="BE"
+                        condition={backendCat}
+                        onClick={BackendHandler}
+                    />
+                    <CategoryComp
+                        onText="Fullstack"
+                        offText="FS"
+                        condition={fullstackCat}
+                        onClick={FullstackHandler}
+                    />
+                    <CategoryComp
+                        onText="ALL"
+                        offText="ALL"
+                        condition={allCat}
+                        onClick={AllHandler}
+                    />
                 </ul>
             </div>
-            <div className='list-skill'>
-                <SkillList />
-
+            <div className="list-skill">
+                <SkillList select={selectedCat} />
             </div>
         </section>
-    )
+    );
 }
-
