2338431d499006fc9358e29338406d777c17a00b
Ode Ardika <dirfanardika@gmail.com>
2025-06-11 18:34:00 +0800

    style: make portofolio card wider at desktop view

--- components/Card/PortfolioCard/PortfolioCard.tsx
+++ components/Card/PortfolioCard/PortfolioCard.tsx
@@ -4,11 +4,11 @@ import { PortfolioCardProps } from '@/types/portfolioCard.types';
 
 export default function PortfolioCard({props} : {props: PortfolioCardProps}) {
   return (
-    <div className='bg-white rounded-[10px] w-80'>
+    <div className='bg-white rounded-[10px] w-100'>
         <Image src={props.image} alt="Project image" className='p-4 rounded-[20px]'/>
         <div className='px-4 pb-4'>
         <h3 className="text-lg font-semibold">{props.title}</h3>
-        <a className="text-sm font-light" href={props.link} target="_blank" rel="noopener noreferrer">Open</a>
+        <a className="text-sm font-light hover:underline hover:text-gray-500" href={props.link} target="_blank" rel="noopener noreferrer">Open</a>
         </div>
     </div>
   )
