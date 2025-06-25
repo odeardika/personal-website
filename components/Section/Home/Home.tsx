78f0f1cf8d3e4693ede9e729b963b96e80b3d4e1
Ode Ardika <dirfanardika@gmail.com>
2025-06-25 15:21:09 +0800

    feat: adding animation on scroll for the page

--- components/Section/Home/Home.tsx
+++ components/Section/Home/Home.tsx
@@ -4,7 +4,10 @@ import profile from "@public/assets/images/profile_img.png";
 
 export default function Home() {
     return (
-        <section className="flex flex-col-reverse px-10 lg:px-48 xl:px-48 xl:flex-row xl:h-screen items-center gap-4 xl:gap-0 lg:flex-row lg:h-screen py-4">
+        <section 
+        data-aos="fade-up"
+        className="flex flex-col-reverse px-10 lg:px-48 xl:px-48 xl:flex-row xl:h-screen items-center gap-4 xl:gap-0 lg:flex-row lg:h-screen py-4">
+            
             <div className="basis-1/2 flex flex-col gap-10 ">
                 <div className="flex flex-col gap-4">
                     <h1 className="text-4xl xl:text-5xl text-dark_blue font-extrabold xl:font-semibold">Ode Ardika 🖐️</h1>
@@ -15,11 +18,13 @@ export default function Home() {
                     <button className="bg-dark_blue text-white py-4 px-6 text-base font-semibold rounded-xl">Say Hello</button>
                 </div>
             </div>
+
             <div className="flex justify-end grow">
                 <div className="">
                     <Image className="border-2 border-gray_blue rounded-full object-cover h-72 w-52 lg:h-96 lg:w-72 xl:h-96 xl:w-72 blob-3 bg-white " src={profile} alt="profile" width={700} height={700} />
                 </div>
             </div>
+
         </section>
     )
 }
