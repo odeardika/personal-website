7b3d8bb90fe927e19625387f3242e63d68f3c8af
Ode Ardika <dirfanardika@gmail.com>
2025-06-26 23:10:21 +0800

    style: update button style on homesection

--- components/Section/Home/Home.tsx
+++ components/Section/Home/Home.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Image from 'next/image';
 import profile from "@public/assets/images/profile_img.png";
 import { TypeAnimation } from 'react-type-animation';
+import { motion } from 'motion/react';
 
 
 
@@ -24,7 +25,7 @@ export default function Home() {
             data-aos="fade-up"
             className="flex flex-col-reverse px-10 lg:px-48 xl:px-48 xl:flex-row xl:h-screen items-center gap-4 xl:gap-0 lg:flex-row lg:h-screen py-4">
 
-            <div className="basis-7/12 flex flex-col gap-10 ">
+            <div className="basis-7/12 flex flex-col gap-6 ">
                 <div className="flex flex-col gap-2">
                     <h1 className="text-4xl xl:text-5xl text-dark_blue font-extrabold xl:font-semibold">Hi, I’m Ode <span className='wave'>👋</span></h1>
                     <div>
@@ -48,12 +49,15 @@ export default function Home() {
                             />
                         </div>
                     </div>
-
-
                     <p className="text-xl font-thin text-slate-400">I design, build, and maintain applications that are scalable, efficient, and user-friendly.</p>
                 </div>
                 <div>
-                    <button className="bg-dark_blue text-white py-4 px-6 text-base font-semibold rounded-2xl">Say Hello</button>
+                    <motion.button
+                        whileHover={{ scale: 1.2 }}
+                        whileTap={{ scale: 0.8, }}
+                        className="bg-dark_blue text-white py-4 px-6 text-base font-semibold rounded-2xl">
+                        Say Hello
+                    </motion.button>
                 </div>
             </div>
 
