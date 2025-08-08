471b15a194a6524cb456676716c18188091187e5
Ode Ardika <dirfanardika@gmail.com>
2025-08-08 22:21:17 +0800

    fix: fix the image in about section

--- components/Section/Project/Project.tsx
+++ components/Section/Project/Project.tsx
@@ -15,8 +15,8 @@ export default function Project() {
   }, [])
   return (
     <section 
-    // data-aos="fade-up" 
-    id='project-section' className='flex flex-col items-center justify-center gap-8 py-24 bg-zinc-50 z-20'>
+    data-aos="fade-up" 
+    id='project-section' className='flex flex-col items-center justify-center gap-8 py-24 bg-zinc-50'>
         <div className="max-w-screen-2xl mx-auto w-full">
             <div className='flex flex-col gap-2 md:gap-4 items-center'>
                 <h2 className='font-semibold text-2xl md:text-4xl lg:text-5xl text-dark_blue'>My Portfolio</h2>
